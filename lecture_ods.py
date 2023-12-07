ce2cebe0dd6beff3feea9b2268e11ec7141550df
François SP <francois-sp@gmx.fr>
2023-12-07 11:44:24 +0100

    Ajout main

--- lecture_ods.py
+++ lecture_ods.py
@@ -2,3 +2,10 @@ import ezodf as ez #pip install ezodf
 from lecture_tableur import Tableur
 class TableurODS(Tableur):
     #TODO: Implémenter les méthodes de la classe Tableur
+
+if __name__ == "__main__":
+    tab = TableurODS("test.xlsx")
+    print(tab.get_entete())
+    print(tab.get_content_colum(0))
+    print(tab.get_content_colum("Titre"))
+    print(tab.get_index_of("Titre"))
